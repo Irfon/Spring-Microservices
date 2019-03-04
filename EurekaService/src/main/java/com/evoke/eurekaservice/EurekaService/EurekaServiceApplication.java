8038c19bcfe5db8be29db65dacc2bb86a87ef874
Irfon <md.irfonbaig@gmail.com>
2019-03-04 23:05:29 +0530

    Update EurekaServiceApplication.java

--- EurekaService/src/main/java/com/evoke/eurekaservice/EurekaService/EurekaServiceApplication.java
+++ EurekaService/src/main/java/com/evoke/eurekaservice/EurekaService/EurekaServiceApplication.java
@@ -10,6 +10,6 @@ public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
-		//updated new line in repo1
+		//updated new line in repo2
 	}
 }
