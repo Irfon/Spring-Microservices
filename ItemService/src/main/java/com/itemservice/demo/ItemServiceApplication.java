44bef1ae705245f78ed14d93d7643f1a377cfb18
Irfon <md.irfonbaig@gmail.com>
2019-03-04 23:56:44 +0530

    Update ItemServiceApplication.java

--- ItemService/src/main/java/com/itemservice/demo/ItemServiceApplication.java
+++ ItemService/src/main/java/com/itemservice/demo/ItemServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
-	        System.out.println("Hello World");
+	        System.out.println("Hello World to github");
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 }
