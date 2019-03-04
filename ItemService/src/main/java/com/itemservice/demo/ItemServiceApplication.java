fae6fa70bba5d184fbb98a28e6893653a07f0120
Irfon <md.irfonbaig@gmail.com>
2019-03-05 00:03:33 +0530

    Changes done to local

--- ItemService/src/main/java/com/itemservice/demo/ItemServiceApplication.java
+++ ItemService/src/main/java/com/itemservice/demo/ItemServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
-	        System.out.println("Hello World to github");
+	        System.out.println("Hello World to github new");
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 }
